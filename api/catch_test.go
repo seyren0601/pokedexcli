4c6534c3260688d3847063fad837e56c032b2f35
ThinhNguyen <sorrenw@gmail.com>
2025-04-27 01:59:59 +0700

    fix: Fix TestCatch() function to test multiple cases

--- api/catch_test.go
+++ api/catch_test.go
@@ -1,42 +1,48 @@
 package api_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/seyren0601/pokedexcli/api"
 )
 
 func TestCatch(t *testing.T) {
-	input := []string{"pikachu", "squirtle"}
-	output := []string{"pikachu", "squirtle"}
-	catched := []string{}
+	cases := []struct {
+		input  []string
+		output []string
+	}{
+		{
+			[]string{"pikachu", "squirtle"},
+			[]string{"pikachu", "squirtle"},
+		},
+		{
+			[]string{"abc", "xyz"},
+			[]string{},
+		},
+	}
 
-	for _, name := range input {
-		for { // Retry until success catch
-			if success, _ := api.Catch(name); success {
-				catched = append(catched, name)
-				break
+	for _, c := range cases {
+		testname := fmt.Sprintf("%v", c.input)
+		t.Run(testname, func(t *testing.T) {
+			var catched []string = []string{}
+			for _, name := range c.input {
+				for { // Retry until success catch or
+					success, err := api.Catch(name)
+					if err != nil { // Exception
+						break
+					}
+
+					if success { // Success catch
+						catched = append(catched, name)
+						break
+					}
+				}
 			}
-		}
+			if len(c.output) != len(catched) {
+				t.Errorf("Test case failed.\nExpected catches: %v.\nGot %v catches\n", len(c.output), len(catched))
+			}
+		})
 	}
 
-	if len(output) != len(catched) {
-		t.Errorf("Test case failed.\nExpected catches: %v.\nGot %v catches\n", len(output), len(catched))
-	}
-}
-
-func TestCatchInvalidNames(t *testing.T) {
-	input := []string{"abc", "xyz"}
-	output := []string{}
-	catched := []string{}
-
-	for _, name := range input {
-		if success, _ := api.Catch(name); success {
-			catched = append(catched, name)
-		}
-	}
-
-	if len(output) != len(catched) {
-		t.Errorf("Test case failed.\nExpected catches: %v.\nGot %v catches\n", len(output), len(catched))
-	}
 }
