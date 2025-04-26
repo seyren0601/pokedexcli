1ee46a3de79fe739e869a4a340c200d176bda470
ThinhNguyen <sorrenw@gmail.com>
2025-04-26 15:32:25 +0700

    feat: Implemented caching to REPL pokedex app
    https://www.boot.dev/lessons/f2ba2b87-38fe-467c-abd7-14716f955169

--- api/maparea.go
+++ api/maparea.go
@@ -4,9 +4,12 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+	"time"
+
+	"github.com/seyren0601/pokedexcli/internal/pokecache"
 )
 
-type mapArea struct {
+type MapArea struct {
 	Name string
 	Url  string
 }
@@ -15,7 +18,7 @@ type mapAreaAPIResponse struct {
 	Count    int
 	Next     string
 	Previous string
-	Results  []mapArea
+	Results  []MapArea
 }
 
 type config struct {
@@ -24,29 +27,35 @@ type config struct {
 }
 
 var Cfg config = config{
-	Next: "https://pokeapi.co/api/v2/location-area/",
+	Next: "https://pokeapi.co/api/v2/location-area/", //
 }
+var cache pokecache.Cache = pokecache.NewCache(5 * time.Second)
 
-func GetMapAreas(url string) ([]mapArea, error) {
-	res, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer res.Body.Close()
+func GetMapAreas(url string) ([]MapArea, error) {
+	var data []byte
+	data, found := cache.Get(url) // Find data in cache first
+	if !found {                   // Can't find data in cache
+		res, err := http.Get(url) // fetch (get) data from Url
+		if err != nil {
+			return nil, err
+		}
+		defer res.Body.Close()
 
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
+		data, err = io.ReadAll(res.Body) // Read data from response body as []byte
+		if err != nil {
+			return nil, err
+		}
+		cache.Add(url, data) // Cache fetched data
 	}
 
 	var content mapAreaAPIResponse
-	err = json.Unmarshal(data, &content)
+	err := json.Unmarshal(data, &content) // Parse data into struct
 	if err != nil {
 		return nil, err
 	}
 
-	Cfg.Previous = content.Previous
-	Cfg.Next = content.Next
+	Cfg.Previous = content.Previous // Set previous page url
+	Cfg.Next = content.Next         // Set next page url
 
 	return content.Results, nil
 }
